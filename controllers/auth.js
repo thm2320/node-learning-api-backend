b272ef1e736a8903c2249d428a12508c6a3d96c6
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-15 10:19:27 +0800

    user signup; encrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -13,6 +14,27 @@ exports.signup = (req, res, next) => {
   const {
     email, name, password
   } = req.body
-
+  bcrypt.hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        email,
+        name,
+        password: hashedPw
+      })
+      return user.save();
+    })
+    .then(result => {
+      res.status(201)
+        .json({
+          message: 'User created',
+          userId: result._id
+        })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err)
+    })
 };
 
