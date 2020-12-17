955144b3ecfa19cf12d813169dbca014fe2d0901
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-17 12:01:33 +0800

    set up for socket.io

--- app.js
+++ app.js
@@ -64,6 +64,11 @@ app.use((err, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(8080)
+    const server = app.listen(8080)
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected')
+    })
+
   })
   .catch(err => console.log(err));
