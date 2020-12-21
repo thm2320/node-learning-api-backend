320b2d16efd140197fe5996be255727d46245a9b
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-21 11:10:46 +0800

    socket io for delete post

--- controllers/feed.js
+++ controllers/feed.js
@@ -187,6 +187,11 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId)
     user.posts.pull(postId);
     await user.save()
+    io.getIO()
+      .emit('posts', {
+        action: 'delete',
+        posts: postId
+      })
     res.status(200)
       .json({
         message: 'Post deleted'
