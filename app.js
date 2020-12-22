042229de47724c996c3d9af738d57389e89ebbf4
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-22 11:11:30 +0800

    return the formatted data when throwing error in resolver

--- app.js
+++ app.js
@@ -52,7 +52,20 @@ app.use('/graphql',
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred';
+      const code = err.originalError.code || 500;
+      return {
+        message: message,
+        status: code,
+        data: data
+      }
+    }
   })
 )
 
