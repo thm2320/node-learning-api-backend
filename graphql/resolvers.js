c2a6f1a62f605fa6583f8496a93f8cd77dd3875e
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-22 11:10:39 +0800

    install validator; implement validation in resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,25 @@
 const bcrypt = require('bcryptjs');
+const { default: validator } = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'E-mail is invalid' });
+    }
+    if (validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 4 })
+    ) {
+      errors.push({ message: 'Password too short' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid Input');
+      throw error;
+    }
+
+
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error('User exists already!');
