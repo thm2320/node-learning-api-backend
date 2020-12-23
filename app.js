4045bcc6bba059fb9f307c0e6efacfec8dbbb418
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-12-23 11:03:45 +0800

    rest upload file before graphql

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser')
@@ -54,6 +55,19 @@ app.use((req, res, next) => {
 
 app.use(auth)
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res.status(201).json({ message: 'File stored', filePath: req.file.path.replace(/\\/g, "/") })
+});
+
 app.use('/graphql',
   graphqlHTTP({
     schema: graphqlSchema,
@@ -92,3 +106,8 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
+}
